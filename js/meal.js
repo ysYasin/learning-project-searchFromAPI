56bb50c01384f6f4e6e9bee471b7d69f4f5c87da
ys yasin arafat <ysyasin7.6@gmail.com>
2023-07-24 19:33:55 +0600

    final

--- js/meal.js
+++ js/meal.js
@@ -11,7 +11,7 @@ const placingCard = (meals) => {
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
-    <div class="card">
+    <div class="card" onclick="loadmealDetiels('${meal.idMeal}')">
       <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
       <div class="card-body">
           <h5 class="card-title">${meal.strMeal}</h5>
@@ -30,3 +30,30 @@ document.getElementById("Search").addEventListener("click", () => {
   loadfood(searchValue);
   search.value = "";
 });
+
+const loadmealDetiels = (idmeal) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idmeal}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => placeMealDetiels(data.meals));
+};
+const placeMealDetiels = (fodds) => {
+  const foodDetielsContainer = document.getElementById("foodDetiels-container");
+  foodDetielsContainer.innerHTML = " ";
+  fodds.forEach((food) => {
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.classList.add("mx-auto");
+    div.classList.add("mt-5");
+    div.style.width = "50%";
+    div.innerHTML = `
+    <div class="card mx-auto" onclick="loadmealDetiels('${food.idMeal}')">
+      <img src="${food.strMealThumb}" class="card-img-top" alt="...">
+      <div class="card-body">
+          <h5 class="card-title">${food.strMeal}</h5>
+          <p class="card-text">${food.strInstructions}</p>
+          </div>
+      </div>`;
+    foodDetielsContainer.appendChild(div);
+  });
+};
